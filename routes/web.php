adefe812cf5b82a5f46b5f60d427549097c15ea7
santstyle <adjixsantosoputra@gmail.com>
2025-08-27 13:01:20 +0700

    Controller

--- routes/web.php
+++ routes/web.php
@@ -36,7 +36,7 @@ Route::get('/contact', function () {
 //     return view('dashboard');
 // })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/dashboard', [PostDashboardController::class, 'showAll'])->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', [PostDashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
