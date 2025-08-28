40fb286ea1cc0ff4b8cfdb72110fb5a5b5493730
santstyle <adjixsantosoputra@gmail.com>
2025-08-28 12:28:33 +0700

    Create Post Dashboard Form

--- app/Http/Controllers/PostDashboardController.php
+++ app/Http/Controllers/PostDashboardController.php
@@ -28,7 +28,7 @@ class PostDashboardController extends Controller
      */
     public function create()
     {
-        //
+        return view('dashboard.create');
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -38,6 +38,8 @@ Route::get('/contact', function () {
 
 Route::get('/dashboard', [PostDashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/dashboard/create', [PostDashboardController::class, 'create'])->middleware(['auth', 'verified']);
+
 Route::get('/dashboard/{post:slug}', [PostDashboardController::class, 'show'])->middleware(['auth', 'verified']);
 
 Route::middleware('auth')->group(function () {
